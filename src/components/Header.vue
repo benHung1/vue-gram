131ead560cfe44ea61fdddd6216c429535db9968
ga2006206521 <ga2006206521@gmail.com>
2022-06-21 18:55:18 +0800

    feat/index

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div class="header flex justify-between w-full p-4 bg-red-300">
+    <div class="flex items-center">
+      <img src="../assets/logo.png" alt="" class="w-10" />
+      <p class="hidden">hihihihi</p>
+    </div>
+    <div>right</div>
+  </div>
+</template>
+
+<style scoped></style>
